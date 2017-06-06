3ff5c1c558f2ef51dc5831adb04042fc103ab9ce
promethwill88 <wfong.bk@gmail.com>
2017-06-06 15:20:44 -0700

    whatever

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,6 +7,7 @@
 angular
   .module('tunely', ['ngRoute'])
   .config(config);
+  
 
 config.$inject = ['$routeProvider', '$locationProvider'];
 
--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,23 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+
+
+vm.deleteSong = function (song) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+ $routeParams.id + '/songs/' + song._id
+    }).then(function successCallback(json) {
+      var index = vm.album.songs.indexOf(song);
+      vm.album.songs.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+
+
+
+
+
+
 }
